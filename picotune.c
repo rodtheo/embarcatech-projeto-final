28359462dc206d6a02a9553a4cabf98b226b0b0b
rodtheo <theodoro.biotec@gmail.com>
2025-02-23 19:03:45 -0300

    FFT funcionando e freq detectada mostrada no display

--- picotune.c
+++ picotune.c
@@ -8,6 +8,18 @@
 #include "hardware/i2c.h"
 #include <string.h>
 
+#include <math.h>
+
+
+typedef signed int fix15 ;
+#define multfix15(a,b) ((fix15)((((signed long long)(a))*((signed long long)(b)))>>15))
+#define float2fix15(a) ((fix15)((a)*32768.0)) // 2^15
+#define fix2float15(a) ((float)(a)/32768.0)
+#define absfix15(a) abs(a) 
+#define int2fix15(a) ((fix15)(a << 15))
+#define fix2int15(a) ((int)(a >> 15))
+#define char2fix15(a) (fix15)(((fix15)(a)) << 15)
+
 
 //// Configuração do ADC ////
 // Canal do ADC e pino
@@ -18,7 +30,7 @@
 // Taxa de amostragem (Hz)
 #define Fs 10000.0
 // Taxa de clock do ADC (imutável!)
-#define ADCCLK 480000.0
+#define ADCCLK 48000000.0
 
 // Canais DMA para amostragem do ADC
 int sample_chan;
@@ -38,6 +50,108 @@ uint8_t* sample_address_pointer = &sample_array[0];
 
 #define I2C_SDA 14
 #define I2C_SCL 15
+
+//// Configuração do FFT ////
+// Número de pontos da FFT menos 1
+#define NUM_SAMPLES_M_1 1023
+// log2 do numero de amostras coletadas
+#define LOG2_NUM_SAMPLES 10
+// tamanho do short (16 bits) menos log2 do número de amostras
+#define SHIFT_AMOUNT 6
+
+
+// array com a parte real das amostras
+fix15 fr[NUM_SAMPLES];
+// array com a parte imaginária das amostras
+fix15 fi[NUM_SAMPLES];
+
+fix15 Sinewave[NUM_SAMPLES];
+fix15 window[NUM_SAMPLES];
+
+void FFTfix(fix15 fr[], fix15 fi[]) {
+
+    unsigned short m;   // one of the indices being swapped
+    unsigned short mr ; // the other index being swapped (r for reversed)
+    fix15 tr, ti ; // for temporary storage while swapping, and during iteration
+
+    int i, j ; // indices being combined in Danielson-Lanczos part of the algorithm
+    int L ;    // length of the FFT's being combined
+    int k ;    // used for looking up trig values from sine table
+
+    int istep ; // length of the FFT which results from combining two FFT's
+
+    fix15 wr, wi ; // trigonometric values from lookup table
+    fix15 qr, qi ; // temporary variables used during DL part of the algorithm
+
+    //////////////////////////////////////////////////////////////////////////
+    ////////////////////////// BIT REVERSAL //////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////
+    // Bit reversal code below based on that found here: 
+    // https://graphics.stanford.edu/~seander/bithacks.html#BitReverseObvious
+    for (m=1; m<NUM_SAMPLES_M_1; m++) {
+        // swap odd and even bits
+        mr = ((m >> 1) & 0x5555) | ((m & 0x5555) << 1);
+        // swap consecutive pairs
+        mr = ((mr >> 2) & 0x3333) | ((mr & 0x3333) << 2);
+        // swap nibbles ... 
+        mr = ((mr >> 4) & 0x0F0F) | ((mr & 0x0F0F) << 4);
+        // swap bytes
+        mr = ((mr >> 8) & 0x00FF) | ((mr & 0x00FF) << 8);
+        // shift down mr
+        mr >>= SHIFT_AMOUNT ;
+        // don't swap that which has already been swapped
+        if (mr<=m) continue ;
+        // swap the bit-reveresed indices
+        tr = fr[m] ;
+        fr[m] = fr[mr] ;
+        fr[mr] = tr ;
+        ti = fi[m] ;
+        fi[m] = fi[mr] ;
+        fi[mr] = ti ;
+    }
+
+    //////////////////////////////////////////////////////////////////////////
+    ////////////////////////// Danielson-Lanczos //////////////////////////////
+    //////////////////////////////////////////////////////////////////////////
+    // Adapted from code by:
+    // Tom Roberts 11/8/89 and Malcolm Slaney 12/15/94 malcolm@interval.com
+    // Length of the FFT's being combined (starts at 1)
+    L = 1 ;
+    // Log2 of number of samples, minus 1
+    k = LOG2_NUM_SAMPLES - 1 ;
+    // While the length of the FFT's being combined is less than the number of gathered samples
+    while (L < NUM_SAMPLES) {
+        // Determine the length of the FFT which will result from combining two FFT's
+        istep = L<<1 ;
+        // For each element in the FFT's that are being combined . . .
+        for (m=0; m<L; ++m) { 
+            // Lookup the trig values for that element
+            j = m << k ;                         // index of the sine table
+            wr =  Sinewave[j + NUM_SAMPLES/4] ; // cos(2pi m/N)
+            wi = -Sinewave[j] ;                 // sin(2pi m/N)
+            wr >>= 1 ;                          // divide by two
+            wi >>= 1 ;                          // divide by two
+            // i gets the index of one of the FFT elements being combined
+            for (i=m; i<NUM_SAMPLES; i+=istep) {
+                // j gets the index of the FFT element being combined with i
+                j = i + L ;
+                // compute the trig terms (bottom half of the above matrix)
+                tr = multfix15(wr, fr[j]) - multfix15(wi, fi[j]) ;
+                ti = multfix15(wr, fi[j]) + multfix15(wi, fr[j]) ;
+                // divide ith index elements by two (top half of above matrix)
+                qr = fr[i]>>1 ;
+                qi = fi[i]>>1 ;
+                // compute the new values at each index
+                fr[j] = qr - tr ;
+                fi[j] = qi - ti ;
+                fr[i] = qr + tr ;
+                fi[i] = qi + ti ;
+            }    
+        }
+        --k ;
+        L = istep ;
+    }
+}
 
 int main()
 {
@@ -150,7 +264,10 @@ int main()
     uint8_t ssd[ssd1306_buffer_length];
     memset(ssd, 0, ssd1306_buffer_length);
     render_on_display(ssd, &frame_area);
-printf("Iniciando captura\n");
+    printf("Iniciando captura\n");
+
+    char text_freq[50];
+    
     char *text[] = {
         "  Iniciando   ",
         "  captura dos ",
@@ -164,27 +281,94 @@ printf("Iniciando captura\n");
     }
     render_on_display(ssd, &frame_area);
 
-    while (true) {
-    
-    // Aguarda o DMA terminar
-    dma_channel_wait_for_finish_blocking(sample_chan);
-    // Reseta os ponteiros do DMA
-    // dma_channel_set_read_addr(control_chan, &sample_address_pointer, false);
-    // Reinicia o DMA
-    // dma_channel_start(control_chan);
+    // FFT
+    // Declare some static variables
+    static int height ;             // for scaling display
+    static float max_freqency ;     // holds max frequency
+    static int i ;                  // incrementing loop variable
 
-    printf("----\n");
-    // Imprime as primeiras 10 amostras
-    for (int i = 0; i < NUM_SAMPLES; i++) {
-    printf("%d\n", sample_array[i]);
+    static fix15 max_fr ;           // temporary variable for max freq calculation
+    static int max_fr_dex ;         // index of max frequency
+
+    // Max and min macros
+    #define max(a,b) ((a>b)?a:b)
+    #define min(a,b) ((a<b)?a:b)
+
+    // 0.4 in fixed point (used for alpha max plus beta min)
+    fix15 zero_point_4 = float2fix15(0.4) ;
+
+    // Populate the sine table and Hann window table
+    int ii;
+    for (ii = 0; ii < NUM_SAMPLES; ii++) {
+        Sinewave[ii] = float2fix15(sin(6.283 * ((float) ii) / (float)NUM_SAMPLES));
+        window[ii] = float2fix15(0.5 * (1.0 - cos(6.283 * ((float) ii) / ((float)NUM_SAMPLES))));
     }
-    printf("----\n");
 
-    // Reinicia o canal de amostras, agora que temos nossa cópia das amostras
-    dma_channel_start(control_chan);
+    while (true) {
+        
+        // Aguarda o DMA terminar
+        dma_channel_wait_for_finish_blocking(sample_chan);
+        // Reseta os ponteiros do DMA
+        // dma_channel_set_read_addr(control_chan, &sample_address_pointer, false);
+        // Reinicia o DMA
+        // dma_channel_start(control_chan);
 
-    // Dorme por 1 segundo
-    sleep_ms(1000);
+        // Copy/window elements into a fixed-point array
+        for (i=0; i<NUM_SAMPLES; i++) {
+            fr[i] = multfix15(int2fix15((int)sample_array[i]), window[i]) ;
+            fi[i] = (fix15) 0 ;
+        }
+
+        // Zero max frequency and max frequency index
+        max_fr = 0 ;
+        max_fr_dex = 0 ;
+
+        // Reinicia o canal de amostras, agora que temos nossa cópia das amostras
+        dma_channel_start(control_chan);
+
+        
+        // Compute the FFT
+        FFTfix(fr, fi) ;
+
+        // Find the magnitudes (alpha max plus beta min)
+        for (int i = 0; i < (NUM_SAMPLES>>1); i++) {  
+            // get the approx magnitude
+            fr[i] = abs(fr[i]); 
+            fi[i] = abs(fi[i]);
+            // reuse fr to hold magnitude
+            fr[i] = max(fr[i], fi[i]) + 
+                    multfix15(min(fr[i], fi[i]), zero_point_4); 
+
+            // Keep track of maximum
+            if (fr[i] > max_fr && i>4) {
+                max_fr = fr[i] ;
+                max_fr_dex = i ;
+            }
+        }
+        // Compute max frequency in Hz
+        max_freqency = max_fr_dex * (Fs/NUM_SAMPLES) ;
+
+        printf("----\n");
+        // Imprime as primeiras 10 amostras
+        // for (int i = 0; i < NUM_SAMPLES; i++) {
+        // printf("%d\n", sample_array[i]);
+        // }
+
+        // free(text_freq);
+        memset(ssd, 0, ssd1306_buffer_length);
+        render_on_display(ssd, &frame_area);
+
+        float diff_to_mi = 330.0 - max_freqency;
+        sprintf(text_freq, "Freq: %d Hz",(int)max_freqency);
+        y = 0;
+        ssd1306_draw_string(ssd, 5, y, text_freq);
+        render_on_display(ssd, &frame_area);
+        
+        printf("%f\n", max_freqency);
+        printf("----\n");
+
+        // Dorme por 1 segundo
+        sleep_ms(250);
     
     }
 
